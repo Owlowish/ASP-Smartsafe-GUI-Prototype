570f9cf2b3beaef4dcd7b36d94f086ff6e04ec66
Yuhan <Yuhan.chen@fichetgroup.com>
2019-07-24 15:00:57 +0200

    f

---
+++ Controllers/SmartsafeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP_SmartSafe.Models;
+
+namespace ASP_SmartSafe.Controllers
+{
+    public class HomeController : Controller
+    {
+ 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
